cdf6003d81ea95602e403634c90fe1856b6a0c6d
korsak <korsak73@mail.ru>
2020-05-05 18:36:22 +0300

    first commit

--- SAService/Properties/AssemblyInfo.cs
+++ SAService/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b75ab110-bee6-4f53-9f92-ebc0611772d9")]
+[assembly: Guid("0e3ade6d-a55a-4784-9967-cfee9d660ded")]
 
 // Version information for an assembly consists of the following four values:
 //
